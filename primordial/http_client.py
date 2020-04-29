c34433ee76b30367dfac39134b0c2950efb374d7
Arief Rohman <didadadida93@gmail.com>
2020-04-29 10:10:32 +0700

    Simplyfy class HttpClient
    * make it more cleary to setup HttpClient with proxies
    * remove unnecessary setting when sent a requests

--- primordial/http_client.py
+++ primordial/http_client.py
@@ -1,13 +1,12 @@
 import requests
 
-
 class HttpClient:
     def __init__(self, **kwargs):
         self.headers = kwargs.pop('headers', {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0'})
         self.cookies = kwargs.pop('cookies', requests.cookies.RequestsCookieJar())
         self.timeout = kwargs.pop('timeout', 20.0)
-        self.hooks = kwargs.pop('hooks', {'response': (self._update_cookies,)})
-        self.kwargs = kwargs
+        self.proxies = kwargs.pop('proxies', None)
+        self._hooks = {'response': (self._update_cookies,)}
 
     def _update_cookies(self, r, *args, **kwargs):
         self.cookies.update(r.cookies)
@@ -16,17 +15,17 @@ class HttpClient:
         headers = kwargs.pop('headers', self.headers)
         cookies = kwargs.pop('cookies', self.cookies)
         timeout = kwargs.pop('timeout', self.timeout)
-        hooks = kwargs.pop('hooks', self.hooks)
-        kwarg = self.kwargs.copy()
-        kwarg.update(kwargs)
+        proxies = kwargs.pop('proxies', self.proxies)
+        hooks = kwargs.pop('hooks', self._hooks)
         with requests.request(
                 method=method,
                 url=url,
                 headers=headers,
                 cookies=cookies,
                 timeout=timeout,
+                proxies=proxies,
                 hooks=hooks,
-                **kwarg) as r:
+                **kwargs) as r:
             return r
 
     def post(self, url, **kwargs):
@@ -34,6 +33,3 @@ class HttpClient:
 
     def get(self, url, **kwargs):
         return self._request('GET', url, **kwargs)
-
-    def update_setup(self, **kwargs):
-        self.kwargs.update(kwargs)
